34fe7aeddbea80d444fbbf1f7f75aeefce3c99bc
razvanpojar79 <razvanpojar79@gmail.com>
2026-01-09 14:50:53 +0200

    Add project files.

---
+++ ServiceAuto.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceAuto.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ServiceAuto.Mobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ServiceAuto.Mobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ServiceAuto.Mobile.Pages")]
